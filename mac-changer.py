f635fdf4c80938d6fcef80383d4df6324bc539ad
redikx@gmail.com <Radek_1974>
2020-06-07 00:26:56 -0400

    add checks if interface and mac exists as parameters

--- mac-changer.py
+++ mac-changer.py
@@ -11,7 +11,12 @@ def get_parameters():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC")
     parser.add_option("-m", "--mac", dest="new_mac", help="New Mac of interface")
-    return parser.parse_args()
+    options, argumemnts = parser.parse_args()
+    if not options.interface:
+        parser.error("Specify interface using -i or --inteface option")
+    elif not options.new_mac:
+        parser.error("Please specify new mac using -m or --mac")
+    return options
 
 def change_mac(interface,new_mac):
     print("[+] Interface " + interface + " changing MAC to " + new_mac + " ...")
@@ -19,7 +24,7 @@ def change_mac(interface,new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-(option, argument) = get_parameters()
+option = get_parameters()
 change_mac(option.interface,option.new_mac)
 
 subprocess.call("ifconfig " + option.interface  + " | grep ether",shell=True)
