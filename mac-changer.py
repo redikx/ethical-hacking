a161c73acca9812b8c8194635a2037c4157f5709
redik <redikx@gmail.com>
2020-08-25 16:56:09 -0400

    plus

--- mac-changer.py
+++ mac-changer.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python
 # Script to change MAC address of specified interface
 
-import subprocess
+
 import optparse
+import subprocess
+import re
 
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC")
-parser.add_option("-m", "--mac", dest="new_mac", help="New Mac of interface")
-(option, argument) = parser.parse_args()
+def get_parameters():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac of interface")
+    options, arguments = parser.parse_args()
+    if not options.interface:
+        parser.error("Specify interface using -i or --inteface option")
+    return options
 
-interface = option.interface;
-new_mac = option.new_mac;
+def get_mac(interface):
+    mac_check = subprocess.check_output(['ifconfig', interface]).decode()
+    mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w" , mac_check)
+    if mac:
+        return mac.group(0)
+    else:
+        return "No MAC found"
 
-print("Interface " + interface + " changing MAC to " + new_mac + " ...")
-subprocess.call(["ifconfig",interface,"down"])
-subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
-subprocess.call(["ifconfig",interface,"up"])
-subprocess.call(["ifconfig",interface])
+def change_mac(interface, new_mac):
+    print("Changing MAC of " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig",interface,"down"])
+    subprocess.call(["ifconfig", interface,"hw","ether",new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+def main():
+    options = get_parameters()
+    interface = options.interface
+    mc_curr_addr = get_mac(interface)
+    print("Current MAC is " + mc_curr_addr)
+    if options.new_mac:
+        new_mac = options.new_mac
+        change_mac(interface,new_mac)
+        mc_curr_addr = get_mac(interface)
+        print("[+] New MAC is " + mc_curr_addr)
+
+if __name__ == "__main__":
+    main()
